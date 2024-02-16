90d38901fca963a5a4d160d6a3b39a5f7fdfcd29
quyetlv12 <lequyet840500@gmail.com>
2024-02-16 12:25:43 +0700

    responsive mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,92 +1,52 @@
-const btn_next = document.querySelector('.btn__next')
-const btn_pre = document.querySelector('.btn__pre')
-const slides = [
-    "Slide 1",
-    "Slide 2",
-    "Slide 3",
-    "Slide 4",
-    "Slide 5",
-    "Slide 6",
-    "Slide 7",
-    "Slide 8",
-    "Slide 9",
-];
+let slides = document.querySelectorAll('.slides div');
+let slideCountItem = slides.length;
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
 let currentIndex = 0;
 
+for (let index = 0; index < slides.length; index++) {
+    const element = slides[index];
 
-const renderSlides = () => {
-    const slidesContainer = document.querySelector('.slides');
-    slidesContainer.innerHTML = '';
-    slides.forEach((slide, index) => {
-        // if (!checkIsImageUrl(slide)) {
-        //     const img = document.createElement('img');
-        //     img.src = slide;
-        //     img.alt = `Slide ${index + 1}`;
-        //     slidesContainer.appendChild(img);
-        // }else{
-           
-        // }
-        const span = document.createElement('span');
-        span.innerHTML = slide;
-        span.alt = `Slide ${index + 1}`;
-        slidesContainer.appendChild(span);
-      
-        
-    });
-    showSlide(currentIndex);
+    element.style.transform = "translateX(" + 100 * (index) + "%)";
 }
+let loop = 0 + 1000 * slideCountItem;
 
-const showSlide = (index) => {
+const checkButtonPrevAndNextDisabled = (index = 0) => {
     if (index === 0) {
-        btn_pre.disabled = true
+        prev.disabled = true
     }
     if (index === slides.length - 1) {
-        btn_next.disabled = true
+        next.disabled = true
     }
-    const slideImages = document.querySelectorAll('.slides span');
-    if (index < 0) {
-        currentIndex = slideImages.length - 1;
-    } else if (index >= slideImages.length) {
-        currentIndex = 0;
-    } else {
-        currentIndex = index;
-    }
-
-    slideImages.forEach((slide, index) => {
-        if (index === currentIndex) {
-            slide.style.display = 'block';
-        } else {
-            slide.style.display = 'none';
-        }
-    });
-    slideImages.forEach((slide) => {
-        const slideWidth = slides[0].clientWidth;
-        const offset = -index * slideWidth;
-        slide.style.transform = `translateX(${offset}px)`;
-        slide.classList.remove('slideInRight' , 'slideInLeft');
-    });
 }
-
-//prev 
-const previousSlide = () => {
-    const slideImages = document.querySelectorAll('.slides span');
-    let _curent_slide = currentIndex
-    btn_next.disabled = false
-    _curent_slide = Math.max(_curent_slide - 1, 0);
-    slideImages[_curent_slide].style.animation = 'slideInLeft 0.2s forwards';
-    showSlide(_curent_slide);
-}
-
 //next slide
-const nextSlide = () => {
-    const slideImages = document.querySelectorAll('.slides span');
+function goNextSlide() {
+    loop++;
     let _curent_slide = currentIndex
-    btn_pre.disabled = false
-    _curent_slide = Math.min(currentIndex + 1, slides.length - 1);
-    slideImages[_curent_slide].style.animation = 'slideInRight 0.2s forwards';
-    showSlide(_curent_slide);
+    prev.disabled = false
+    _curent_slide = Math.max(_curent_slide + 1, 0);
+    checkButtonPrevAndNextDisabled(_curent_slide)
+    currentIndex = _curent_slide
+    for (let index = 0; index < slides.length; index++) {
+        const element = slides[index];
+        element.style.transform = "translateX(" + 100 * (index - loop % slideCountItem) + "%)";
+    }
+
 }
+//previous slide
+function goPrevSlide() {
+    loop--;
+    let _curent_slide = currentIndex
+    next.disabled = false
+    _curent_slide = Math.max(_curent_slide - 1, 0);
+    checkButtonPrevAndNextDisabled(_curent_slide)
+    currentIndex = _curent_slide
+    for (let index = 0; index < slides.length; index++) {
+        const element = slides[index];
+        element.style.transform = "translateX(" + 100 * (index - loop % slideCountItem) + "%)";
+    }
+}
+// load check button when load page
+document.addEventListener('DOMContentLoaded', checkButtonPrevAndNextDisabled(currentIndex));
 
 
-
-document.addEventListener('DOMContentLoaded', renderSlides);
